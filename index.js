5600af3eeea1894af7b8e43e62c482fe0ac221ae
Riadh Mouamnia <riadh.mouamnia@gmail.com>
2023-08-04 15:42:07 +0100

    COopleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+// no longer has DOM node 'main#main'
+const main = document.getElementById("main");
+main.remove();
+
+// has a 'newHeader' variable that points to an <h1> node
+const newHeader = document.createElement("h1");
+// the 'newHeader' variable that points to the <h1> node has an id of 'victory'
+newHeader.id = "victory";
+
+// the 'newHeader' variable that points to the <h1> node with an id of 'victory' has the text "YOUR-NAME is the champion"
+newHeader.innerHTML = "Menar is the champion";
+
+document.body.appendChild(newHeader);
